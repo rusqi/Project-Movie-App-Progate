9f50f66ea87a7527d0f1cc0990e86a32d6b40c8b
Muhammad Rusqi <muhammadrusqi10@gmail.com>
2024-05-27 09:13:18 +0700

    Set up bottom tab navigation

---
+++ src/navigations/BottomTabNavigation.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
+import Home from "/Users/Muhammad Rusqi/movie_app/screens/Home"
+import Search from '/Users/Muhammad Rusqi/movie_app/screens/Search'
+import Favorite from "/Users/Muhammad Rusqi/movie_app/screens/Favorite"
+
+const Tab = createBottomTabNavigator()
+
+const BottomTabNavigator = (): JSX.Element => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="home" size={28} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={Search}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="search" size={28} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Favorite"
+      component={Favorite}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="heart" size={28} color={color} />
+        ),
+        headerShown: false,
+      }}
+    />
+  </Tab.Navigator>
+)
+
+export default BottomTabNavigator
